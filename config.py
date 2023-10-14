947019013518a45d9dbcdc51a36a6bb440b2ead5
Elphador <96861627+Elphador@users.noreply.github.com>
2023-10-14 18:09:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #pass your tokens here 
-BOT_TOKEN ='6028947050:AAHQqySDT8zRIIgMgWBTYUaSs7YcN6q8hwg' # @checktestsbot # bot token from t.me/botfather
+BOT_TOKEN ='6028947050:AAHQqySDT8zRIIgMgWBTYUaSs7YcN6q8hwg' # bot token from t.me/botfather
 API_ID = 9583161 #api id from telegram.org
 API_HASH = 'aecf9e2b7c655c4f916564ab6d598a73' #api hash from telegram.org
     
