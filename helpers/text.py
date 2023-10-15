16015249a68bb894cf3f65d59e6c6fa69bbf5183
Elphador <96861627+Elphador@users.noreply.github.com>
2023-10-15 18:56:29 +0300

    Update text.py

--- helpers/text.py
+++ helpers/text.py
@@ -1,12 +1,18 @@
-HELP = """**--•Examples--\n \n\n•¸Explain quantum computing in simple terms
+HELP = """**--•Examples--\n \n\n•¸Explain quantum computing in simple terms
 •¸Got any creative ideas for a 10 year oldâs birthday?
-•¸How do I make an HTTP request in Javascript?
-\n\n•¸Capabilites\n\n•Remembers what user said earlier in the conversation,
+•¸How do I make an HTTP request in Javascript?
+\n\n•¸Capabilites\n\n•Remembers what user said earlier in the conversation,
 •Allows user to provide follow-up corrections,
 •Trained to decline inappropriate requests\n\n•Limitations\n\nMay occasionally generate incorrect information,
-•¸May occasionally produce harmful instructions or biased content,
-•¸Limited knowledge of world and events after 2021
-•¸may it takes to long for some responses the messages $hello and $genrate me some c++ code can not be treated the same,so depending on your query bot can take  min 3second and max 30 seconds to response
+•¸May occasionally produce harmful instructions or biased content,
+•¸Limited knowledge of world and events after 2021
+•¸may it takes to long for some responses the messages $hello and $genrate me some c++ code can not be treated the same,so depending on your query bot can take  min 3second and max 30 seconds to response
 
 source Code https://github.com/Elphador/ChatGpt-Telegram-Bot
 **"""
+
+
+
+
+
+
