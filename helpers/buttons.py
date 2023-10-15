6f6bc25d3f550f9a708522a49d3bd4a36a44959a
Elphador <96861627+Elphador@users.noreply.github.com>
2023-10-15 18:58:11 +0300

    Update buttons.py

--- helpers/buttons.py
+++ helpers/buttons.py
@@ -3,5 +3,12 @@ from pyrogram.types import InlineKeyboardButton as ikb
 
 channel = ikb("Channel🌴",url='https://t.me/neuralp')
 group = ikb("Group🪺",url='https://t.me/neuralg')
-developer = ikb("Developer🦭",url="https://t.me/e_phador")
+developer = ikb("Developer🦭",url="https://t.me/nextdevil")
 help = ikb("Help📃",callback_data='help')
+
+
+
+
+
+
+
