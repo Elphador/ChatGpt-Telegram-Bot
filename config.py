9657c926065c95fedd27717b132192706d5e61c5
shamil <shamxl@pm.me>
2023-11-21 17:32:49 +0530

    fix: vulnerability
    bot token & api ids visible for visitors

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
+import os
+
 #pass your tokens here 
-BOT_TOKEN ='6028947050:AAHQqySDT8zRIIgMgWBTYUaSs7YcN6q8hwg' # bot token from t.me/botfather
-API_ID = 9583161 #api id from telegram.org
-API_HASH = 'aecf9e2b7c655c4f916564ab6d598a73' #api hash from telegram.org
+BOT_TOKEN = os.getenv("BOT_TOKEN") # bot token from t.me/botfather
+API_ID = os.getenv("API_ID") #api id from telegram.org
+API_HASH = os.getenv("API_HASH") #api hash from telegram.org
     
