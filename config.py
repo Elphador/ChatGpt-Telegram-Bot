cc1cc35768c9add30563c637fa61866987815857
elphador <elphador>
2024-08-23 13:48:40 -0400

    update to v 1.0

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #pass your tokens here 
-BOT_TOKEN ='6028947050:AAHQqySDT8zRIIgMgWBTYUaSs7YcN6q8hwg' # bot token from t.me/botfather
-API_ID = 9583161 #api id from telegram.org
-API_HASH = 'aecf9e2b7c655c4f916564ab6d598a73' #api hash from telegram.org
+BOT_TOKEN ='YOUR BOT TOKERN ' # bot token from t.me/botfather
+API_ID = YOUR API ID #api id from telegram.org
+API_HASH = YOUR API HASH #api hash from telegram.org
     
